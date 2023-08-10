03bab7fd7ccee1bed3cc64a78e761e8c4352e6c5
diego <dmdalera@gmail.com>
2023-08-10 17:39:33 -0300

    first commit

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,55 @@
-        // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-        
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    getDoc,
+    deleteDoc,
+    doc,
+    updateDoc,
+    onSnapshot,
+} from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js'
 
-        const firebaseConfig = {
-            apiKey: "AIzaSyCFEQpgbtB294Z6vUiDvqba0YgQuVpPCOU",
-            authDomain: "turnosenlinea-f601e.firebaseapp.com",
-            projectId: "turnosenlinea-f601e",
-            storageBucket: "turnosenlinea-f601e.appspot.com",
-            messagingSenderId: "755987946873",
-            appId: "1:755987946873:web:95b894bc0a7de86a75e3a8"
-        };
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA-CGvrLFUzDSyAPYdBz9L8HcDXfnNMkG4",
+    authDomain: "turnosestetica-b8640.firebaseapp.com",
+    projectId: "turnosestetica-b8640",
+    storageBucket: "turnosestetica-b8640.appspot.com",
+    messagingSenderId: "909698048504",
+    appId: "1:909698048504:web:c3bbe0da09c207aa9b360d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore();
+
+export const guardarServicio = (servicio, duracion, cantidadTurnos, valor) => {
+    addDoc(collection(db, 'servicios'), { servicio: servicio, duracion: duracion, cantidadTurnos: cantidadTurnos, valor: valor })
+}
+
+export const obtenerServicios = () => getDocs(collection(db, 'servicios'))
+
+export const escucharCambios = () => console.log("escuchar servicios")
+
+export {
+    collection,
+    onSnapshot,
+    db
+}
+
+export const borrarServicio = id => deleteDoc(doc(db, "servicios", id))
+
+//obtengo solo un servicio
+export const obtenerServicio = id => getDoc(doc(db, "servicios", id))
+
+export const actualizarServicio = (id,newField) => 
+updateDoc(doc(db, "servicios", id),newField)
+
--- js/servicios.js
+++ js/servicios.js
@@ -1,172 +1,110 @@
-import { getDatabase, ref, push, onValue, remove, update } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+import { guardarServicio, obtenerServicio, onSnapshot, collection, db, borrarServicio, actualizarServicio } from '../js/firebase.js'
 
-const botonIngresoServicio = document.getElementById("btn_guardar_servicio");
-const botonEditarServicio = document.getElementById("btn_editar_servicio");
-const tablaServicios = document.getElementById("tabla-servicios");
-const cuerpoTablaServicios = document.getElementById("cuerpo-tabla-servicios");
-
-let claveServicioEditar = null;
-let servicioAEditar = null;
+const formAgregarServicio = document.getElementById("formulario-servicio-agregar");
+const btnAgregarServicio = document.getElementById("btn_agregar_servicio")
+const cuerpoTablaServicios = document.getElementById("cuerpo-tabla-servicios")
+let editStatus = false
+let idEdit
 
 //EventListener
-document.addEventListener("DOMContentLoaded", (e) => {
-  cargarServicios();
-})
-
-botonIngresoServicio.addEventListener("click", () => {
-  guardarServicio();
-})
-
-botonEditarServicio.addEventListener("click", () => {
-  editarServicioCargado();
-})
-
-function editarServicio(clave, servicio) {
-  console.log(clave, servicio)
-  claveServicioEditar = clave;
-  servicioAEditar = servicio;
-  window.location = "#modal-editar"
-
-  document.getElementById("nombre_servicio_ed").value = servicio.nombre_servicio;
-  document.getElementById("duracion_ed").value = servicio.duracion;
-  document.getElementById("cantidad_turnos_ed").value = servicio.cantidad_turnos;
-  document.getElementById("valor_ed").value = servicio.precio;
-}
-
-function editarServicioCargado() {
-  const database = getDatabase();
-  const referenciaServicios = ref(database, "servicios");
-
-  var claveRegistro = claveServicioEditar;
-
-  const nombreServicio = document.getElementById("nombre_servicio_ed").value;
-  const duracion = parseInt(document.getElementById("duracion_ed").value);
-  const cantidadTurnos = parseInt(document.getElementById("cantidad_turnos_ed").value);
-  const valor = parseFloat(document.getElementById("valor_ed").value);
-
-  // Para actualizar los datos de un registro específico en Firebase, debemos construir el objeto de actualización con los campos que deseamos cambiar.
-  // En este caso, asumiendo que el registro tiene una clave única, utilizaremos la función `child()` para apuntar a esa clave.
-  // Luego, utilizaremos la función `update()` para realizar la actualización de los campos correspondientes.
-
-  var registroActualizado = {};
-  registroActualizado[`/${claveRegistro}/nombre_servicio`] = nombreServicio;
-  registroActualizado[`/${claveRegistro}/duracion`] = duracion;
-  registroActualizado[`/${claveRegistro}/cantidad_turnos`] = cantidadTurnos;
-  registroActualizado[`/${claveRegistro}/precio`] = valor;
-
-  // Ahora podemos realizar la actualización en la base de datos de Firebase
-  update(referenciaServicios, registroActualizado)
-    .then(function () {
-      alert('Registro actualizado correctamente');
-    })
-    .catch(function (error) {
-      console.error('Error al actualizar el registro:', error);
+document.addEventListener("DOMContentLoaded", async () => {
+  onSnapshot(collection(db, "servicios"), (querySnapshot) => {
+    let html = '';
+    querySnapshot.forEach(doc => {
+      const servicio = doc.data();
+      html += `
+          <tr>
+            <td>${servicio.servicio}</td>
+            <td>${servicio.duracion}</td>
+            <td>${servicio.cantidadTurnos}</td>
+            <td>${servicio.valor}</td>
+            <td>
+              <button class="btn-borrar" doc-id="${doc.id}">Borrar</button>
+              <button class="btn-editar" doc-id="${doc.id}">Editar</button>
+            </td>
+          </tr>
+    `
     });
+    cuerpoTablaServicios.innerHTML = html;
+
+    //agrego event listener borrar
+    const botonesBorrarServicios = document.querySelectorAll(".btn-borrar")
+    console.log(botonesBorrarServicios)
+
+    botonesBorrarServicios.forEach(btn => {
+      btn.addEventListener("click", (event) => {
+        var id = btn.getAttribute('doc-id');
+        console.log(id)
+        borrarServicio(id)
+      })
+    })
+
+    //agrego event listener editar
+    const botonesEditarServicios = document.querySelectorAll(".btn-editar")
+    console.log(botonesEditarServicios);
+
+    botonesEditarServicios.forEach(btn => {
+      btn.addEventListener("click", async (event) => {
+        var id = btn.getAttribute('doc-id');
+
+        const dato = await obtenerServicio(id)
+        console.log(dato.data())
+        const servicioEditar = dato.data()
+
+        let modal = document.getElementById("modal-agregar");
+        modal.style.display = "block";
+
+        editStatus = true
+        idEdit = id;
+
+        btnAgregarServicio.innerText="Actualizar";
+        formAgregarServicio["nombre_servicio_ag"].value = servicioEditar.servicio
+        formAgregarServicio["duracion_ag"].value = servicioEditar.duracion
+        formAgregarServicio["cantidad_turnos_ag"].value = servicioEditar.cantidadTurnos
+        formAgregarServicio["valor_ag"].value = servicioEditar.valor
+      })
+    })
+  })
+})
+
+//Agregar y editar Servicio
+btnAgregarServicio.addEventListener("click", (e) => {
+  e.preventDefault
+  const servicio = document.getElementById("nombre_servicio_ag").value
+  const duracion = parseInt(document.getElementById("duracion_ag").value)
+  const cantidadTurnos = parseInt(document.getElementById("cantidad_turnos_ag").value)
+  const valor = parseFloat(document.getElementById("valor_ag").value)
+
+  if (!editStatus) {
+    guardarServicio(servicio, duracion, cantidadTurnos, valor);
+  } else {
+    actualizarServicio(idEdit, {servicio, duracion, cantidadTurnos, valor})
+    editStatus = false;
+    btnAgregarServicio.innerText="Grabar"
+  }
+
+  formAgregarServicio.reset();
+  modal.style.display="none";
+
+})
+
+// Modal Agregar Servicio
+const agregarServicio = document.getElementById("enlaceAgregarServicios")
+let modal = document.getElementById("modal-agregar");
+let span = document.getElementsByClassName("close")[0];
+
+agregarServicio.onclick = function () {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 
-/*Guarda los servicio en Firebase */
-function guardarServicio() {
-
-  const database = getDatabase();
-  const referenciaServicios = ref(database, "servicios");
-
-  const nombreServicio = document.getElementById("nombre_servicio").value;
-  const duracion = parseInt(document.getElementById("duracion").value);
-  const cantidadTurnos = parseInt(document.getElementById("cantidad_turnos").value);
-  const valor = parseFloat(document.getElementById("valor").value);
-
-  const nuevoServicio = {
-    nombre_servicio: nombreServicio,
-    duracion: duracion,
-    precio: valor,
-    cantidad_turnos: cantidadTurnos
-  };
-
-
-
-  push(referenciaServicios, nuevoServicio)
-    .then(() => {
-      console.log("Servicio agregado correctamente a la base de datos.");
-      document.getElementById("formulario_servicio").reset();
-    })
-    .catch((error) => {
-      console.error("Error al guardar el servicio:", error);
-    });
-}
-
-/* Carga los servicos en La tabla */
-function cargarServicios() {
-
-  const database = getDatabase();
-  const referenciaServicios = ref(database, "servicios");
-
-  // Escuchar cambios en la colección "servicios"
-  onValue(referenciaServicios, (snapshot) => {
-    cuerpoTablaServicios.innerHTML = "";
-
-    if (snapshot.exists()) {
-      const servicios = snapshot.val();
-
-      for (const clave in servicios) {
-        const servicio = servicios[clave];
-
-        const fila = document.createElement("tr");
-
-        // Agregar las celdas con los datos del servicio
-        const celdaNombre = document.createElement("td");
-        celdaNombre.textContent = servicio.nombre_servicio;
-        fila.appendChild(celdaNombre);
-
-        const celdaDuracion = document.createElement("td");
-        celdaDuracion.textContent = servicio.duracion;
-        fila.appendChild(celdaDuracion);
-
-        const celdaCantidadTurnos = document.createElement("td");
-        celdaCantidadTurnos.textContent = servicio.cantidad_turnos;
-        fila.appendChild(celdaCantidadTurnos);
-
-        const celdaPrecio = document.createElement("td");
-        celdaPrecio.textContent = servicio.precio;
-        fila.appendChild(celdaPrecio);
-
-        // Agregar botones de eliminar y editar para cada servicio
-        const celdaAcciones = document.createElement("td");
-        const botonEliminar = document.createElement("button");
-        botonEliminar.textContent = "Eliminar";
-        botonEliminar.addEventListener("click", () => eliminarServicio(clave));
-        celdaAcciones.appendChild(botonEliminar);
-
-        const botonEditar = document.createElement("button");
-        botonEditar.textContent = "Editar";
-        botonEditar.addEventListener("click", () => editarServicio(clave, servicio));
-        celdaAcciones.appendChild(botonEditar);
-
-        fila.appendChild(celdaAcciones);
-        cuerpoTablaServicios.appendChild(fila);
-
-      }
-    } else {
-      const filaVacia = document.createElement("tr");
-      const celdaMensaje = document.createElement("td");
-      celdaMensaje.setAttribute("colspan", "5");
-      celdaMensaje.textContent = "No hay datos en la colección 'servicios'.";
-      filaVacia.appendChild(celdaMensaje);
-      tablaServicios.appendChild(filaVacia);
-    }
-  }, (error) => {
-    console.error("Error al leer datos de la base de datos:", error);
-  });
-}
-
-/*--- Elimina servicios ---*/
-function eliminarServicio(clave) {
-  const servicioAEliminar = ref(getDatabase(), `servicios/${clave}`);
-  remove(servicioAEliminar)
-    .then(() => {
-      console.log("Servicio eliminado correctamente.");
-    })
-    .catch((error) => {
-      console.error("Error al eliminar el servicio:", error)
-    })
-};
