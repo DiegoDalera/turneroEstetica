cd8c103d2e1b0f87763c2e85b07fea8b6e1a67f3
diego <dmdalera@gmail.com>
2023-08-23 19:08:56 -0300

    daypicker add

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ import {
 
 //Constantes
 const arrayDeTurnos = [];
+let esteticistaSeleccionada = undefined;
+let servicioSeleccionado = undefined;
 const selectServicios = document.getElementById("servicios");
 const selectEsteticistas = document.getElementById("esteticistas");
 
@@ -16,11 +18,18 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
 selectServicios.addEventListener('change', (event) => {
-    const servicioSeleccionado = event.target.value;
+    servicioSeleccionado = event.target.value;
     //console.log('Opción seleccionada:', opcionSeleccionada);
     cargarEsteticistas(servicioSeleccionado)
 });
 
+selectEsteticistas.addEventListener('click', (e) => {
+    esteticistaSeleccionada = e.target.value;
+    console.log('Opción seleccionada:', esteticistaSeleccionada);
+    cargarTurnosDisponibles()
+})
+
+//recupera turnos y los carga en un array para trabajar
 async function recuperarTurnos() {
     //traigo todos los turnos y los almaceno en mi array
     const turnos = await obtenerTurnos();
@@ -54,10 +63,9 @@ function cargarServicios() {
 
 }
 
-function cargarEsteticistas(servicioSeleccionado) {
+function cargarEsteticistas() {
 
-
-    // Eliminar todas las opciones
+    // Eliminar todas las opciones del select previas
     while (selectEsteticistas.firstChild) {
         selectEsteticistas.removeChild(selectEsteticistas.firstChild);
     }
@@ -67,16 +75,70 @@ function cargarEsteticistas(servicioSeleccionado) {
     if (objetosEncontrados.length > 0) {
         console.log("Objetos encontrados:", objetosEncontrados);
     } else {
-        console.log("Objetos no encontrados");
+        console.log("Objetos no encontrados - ojo no deberia entrar aqui nunca");
     }
 
-    const nombreEmpleado = objetosEncontrados[0].objetoEmpleado.nombre;
-    console.log(nombreEmpleado)
-    const option = document.createElement("option");
-    option.value = nombreEmpleado;
-    option.textContent = nombreEmpleado;
-    selectEsteticistas.appendChild(option);
-    selectEsteticistas.disabled = false;
-
-
+    objetosEncontrados.forEach(objeto => {
+        const nombreEmpleado = objeto.objetoEmpleado.nombre;
+        console.log(nombreEmpleado);
+        const option = document.createElement("option");
+        option.value = nombreEmpleado;
+        option.textContent = nombreEmpleado;
+        selectEsteticistas.appendChild(option);
+        selectEsteticistas.disabled = false;
+    });
 }
+
+
+function cargarTurnosDisponibles() {
+    console.log(esteticistaSeleccionada)
+    console.log(servicioSeleccionado)
+
+    const conexionBuscada = arrayDeTurnos.find(objeto => objeto.objetoEmpleado.nombre === esteticistaSeleccionada && objeto.objetoServicio.servicio === servicioSeleccionado);
+
+    const diasDeTurnos = conexionBuscada.diasATrabajar;
+
+    const diasHabilitados = diasDeTurnos.map(dia => {
+        switch (dia) {
+            case "Lunes":
+                return 1;
+            case "Martes":
+                return 2;
+            case "Miercoles":
+                return 3;
+            case "Jueves":
+                return 4;
+            case "Viernes":
+                return 5;
+            case "Sabado":
+                return 6;
+            case "Domingo":
+                return 0;
+            default:
+                return null;
+        }
+    })
+
+    const picker = new Pikaday({
+        field: document.getElementById('datepicker'),
+        format: 'YYYY-MM-DD',
+        maxDate: new Date(conexionBuscada.fechaFin),
+        minDate: moment().toDate(),
+        disableDayFn: function (date) {
+            const day = moment(date).day();
+            return !diasHabilitados.includes(day);
+        }
+    });
+
+    picker.show();
+}
+
+//seleccion fecha turno
+const dateInput = document.getElementById('datepicker');
+dateInput.addEventListener('change', function (e) {
+    e.preventDefault
+    const selectedDate = dateInput.value;
+    console.log('Fecha seleccionada:', selectedDate);
+    mostrarHorariosDisponibles()
+});
+
